1e3073ebf4465fc9239edd17cd0840b2c5249a67
Vladislav Chistov <vchistov@ronte.io>
2020-06-03 17:59:33 +0300

    123

--- js/script.js
+++ js/script.js
@@ -1,26 +1,11 @@
 $(document).ready(function(){
 	$('.slider').slick({
-		lazyLoad: 'ondemand',
-		arrows:true,
-		dots:true,
-		slidesToShow:1,
-		autoplay:false,
-		speed:1000,
-		autoplaySpeed:100,
-		responsive:[
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow:2
-				}
-			},
-			{
-				breakpoint: 550,
-				settings: {
-					slidesToShow:1
-				}
-			}
-		]
+		dots: true,
+		
+    	arrows: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		mobileFirst:true,
+		
+	  });
 	});
-});
-
